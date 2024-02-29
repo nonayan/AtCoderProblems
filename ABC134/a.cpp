7b4b71d10fdb49e4d1990778f6f6e7a91c525056
nonayan <kousuke20001219@icloud.com>
2024-02-29 22:29:30 +0900

    add: a.cpp

---
+++ ABC134/a.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+#define INF 1010000000
+
+int main(){
+    int r;
+    cin >> r;
+    cout << 3*r*r << endl;
+}
