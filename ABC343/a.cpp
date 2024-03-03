38d449cc18c53857954e6486b5b2b450e50bab4f
nonayan <kousuke20001219@icloud.com>
2024-03-03 17:16:54 +0900

    add: a.cpp

---
+++ ABC343/a.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+#define INF 1010000000
+
+int main(){
+    int a, b, ans;
+    cin >> a >> b;
+    int sum = a + b;
+    rep(i, 10) {
+        if (sum != i) {
+            ans = i;
+            break;
+        }
+    }
+    cout << ans << endl;
+}
