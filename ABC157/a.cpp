1450c23d06f4b2b5921463b8505d82a64bb43759
nonayan <kousuke20001219@icloud.com>
2024-03-17 19:09:43 +0900

    add: a.cpp

---
+++ ABC157/a.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+#define INF 1010000000
+
+int main(){
+    int n;
+    cin >> n;
+    if (n%2 == 0) cout << n/2 << endl;
+    else cout << n/2 + 1 << endl;
+}
