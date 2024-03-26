8bf784a6547d7068c7fa93eba710802992fdb7e9
nonayan <kousuke20001219@icloud.com>
2024-03-26 22:14:48 +0900

    add: a.cpp

---
+++ ABC169/a.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define INF 1010000000
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    cout << a*b << endl;
+}
