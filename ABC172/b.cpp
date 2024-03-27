db196c86f2a26cf9202fb67ccee7e191bc8fdf37
nonayan <kousuke20001219@icloud.com>
2024-03-27 23:04:09 +0900

    add: b.cpp

---
+++ ABC172/b.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define INF 1010000000
+
+int main(){
+    string s, t;
+    cin >> s >> t;
+    int count = 0;
+    rep(i, s.size()) {
+        if (s[i] != t[i]) count++;
+    }
+    cout << count << endl;
+}
