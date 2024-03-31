9c995c5c8cc9b0f8432af1a4584f5f9bd78192fe
nonayan <kousuke20001219@icloud.com>
2024-03-31 14:19:22 +0900

    fix: c.cpp

--- ABC347/c.cpp
+++ ABC347/c.cpp
@@ -1,39 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define rep(i,n) for (int i = 0; i < (n); ++i)
-using ll = long long;
-#define INF 1010000000
 
 int main() {
-    int N, A, B;
-    cin >> N >> A >> B;
-
-    vector<ll> D(N);
-    rep(i, N) {
-        cin >> D[i];
+    int n, a, b;
+    cin >> n >> a >> b;
+    // 1週間
+    int w = a+b;
+    vector<int> d(n);
+    rep(i, n) {
+        cin >> d[i];
+        d[i] %= w;
     }
-    int count = 0;
-    bool all_holidays = true;
-    for (int i = 0; i < A+B; ++i) {
-        rep(j, N) {
-            int ans;
-            if ((D[j]+i)%(A+B) == 0) ans = A+B;
-            else ans = (D[j]+i)%(A+B);
-            // cout << ans << endl;
-            if (ans > A) {
-            all_holidays = false;
-            break;
-            }
+    sort(d.begin(), d.end());
+    // 2週間
+    rep(i, n) d.push_back(d[i]+w);
+
+    rep(i, d.size()-1) {
+        if (d[i+1]-d[i] >= b+1) {
+            cout << "Yes" << endl;
+            return 0;
         }
-        if (all_holidays) count++;
-        // cout << (all_holidays ? "Yes" : "No") << endl;
     }
-
-    if (count > 0) {
-        cout << "Yes" << endl;
-    } else {
-        cout << "No" << endl;
-    }
-
+    cout << "No" << endl;
     return 0;
 }
